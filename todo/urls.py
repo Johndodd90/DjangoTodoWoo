9073d3ac4098386a4c2f23774ac3ab6e67bb5a16
John Dodd <johndodd90@gmail.com>
2020-04-27 17:46:36 +0100

    user auth system added

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signupuser, name="signupuser")
+    path('signup/', views.signupuser, name="signupuser"),
+    path('logout/', views.logoutuser, name="logoutuser"),
+    path('login/', views.loginuser, name="loginuser"),
+    path('current/', views.currenttodos, name="currenttodos"),
+    path('create/', views.createtodo, name="createtodo"),
+    path('todo/<int:todo_pk>/', views.viewtodo, name="viewtodo"),
+
+
+
 ]
